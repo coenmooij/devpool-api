6909a94fca907fb6541cb240dffd8972eb1eafb3
bqevin <kevin.barasa001@gmail.com>
2018-03-08 23:22:41 +0300

    add form entities

---
+++ src/Developer/Form/Answer.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CoenMooij\DevpoolApi\Developer\Form;
+
+use CoenMooij\DevpoolApi\Developer\Developer;
+use CoenMooij\DevpoolApi\Developer\Question\Question;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Answer extends Model
+{
+    public const ID = 'id';
+    public const DEVELOPER_ID = 'developer_id';
+    public const QUESTION_ID = 'question_id';
+    public const VALUE = 'value';
+
+    public function developer(): BelongsTo
+    {
+        return $this->belongsTo(Developer::class);
+    }
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
+}
