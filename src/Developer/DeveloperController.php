0741493a3517f084c0fc131c9e92fad11af1e862
Coen Mooij <coen.mooij@werkspot.nl>
2018-03-11 12:21:54 +0300

    let developer extend user and return real list of developers

--- src/Developer/DeveloperController.php
+++ src/Developer/DeveloperController.php
@@ -4,48 +4,35 @@ declare(strict_types=1);
 
 namespace CoenMooij\DevpoolApi\Developer;
 
-use CoenMooij\DevpoolApi\Authentication\AuthenticationServiceInterface;
 use CoenMooij\DevpoolApi\Infrastructure\AbstractController;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class DeveloperController extends AbstractController
 {
-    private const SENIORITY = ['trainee', 'junior', 'medior', 'senior', 'lead', 'architect'];
-    private const SPECIALISATION = ['fullstack', 'frontend', 'backend', 'devops', 'mobile'];
-    private const STATUSES = ['code review', 'technical interview', 'caspar', 'code missing'];
+    /**
+     * @var DeveloperServiceInterface
+     */
+    private $developerService;
 
-    public function getAll(): JsonResponse
+    public function __construct(DeveloperServiceInterface $developerService)
     {
-        $developerDataStub = [
-            $this->createDeveloperData('Kevin', 'Barasa', ['PHP', 'Javascript'], ['Laravel', 'Vue'], 0),
-            $this->createDeveloperData('Vincent', 'Wijdeveld', ['C#', 'Java'], ['.NET', 'Spring'], 1),
-            $this->createDeveloperData('Sebastiaan', 'Tan', ['Javascript'], ['React-native'], 2),
-            $this->createDeveloperData('Coen', 'Mooij', ['PHP', 'Javascript'], ['Laravel'], 3),
-            $this->createDeveloperData('Derk', 'van der Grijp', ['Objective-C', 'Python'], ['Django'], 4),
-            $this->createDeveloperData('Abigail', 'Muruchi', ['Swift'], [], 5),
-        ];
-
-        return self::createResponse($developerDataStub, Response::HTTP_OK);
+        $this->developerService = $developerService;
     }
 
-    private function createDeveloperData(
-        string $firstName,
-        string $lastName,
-        array $programmingLanguages,
-        array $frameworks,
-        int $id
-    ): array {
-        return [
-            'id' => $id,
-            'firstName' => $firstName,
-            'lastName' => $lastName,
-            'specialisation' => self::SPECIALISATION[random_int(0, count(self::SPECIALISATION) - 1)],
-            'seniority' => self::SENIORITY[random_int(0, count(self::SENIORITY) - 1)],
-            'programmingLanguages' => $programmingLanguages,
-            'frameworks' => $frameworks,
-            'currentSalary' => random_int(500, 5000),
-            'status' => self::STATUSES[random_int(0, count(self::STATUSES) - 1)],
-        ];
+    public function getAll(Request $request): JsonResponse
+    {
+        // Todo: validate filter by, sort/order
+        $developers = $this->developerService->getAll();
+
+        return self::createResponse(['data' => $developers], Response::HTTP_OK);
+    }
+
+    public function getOne(int $id): JsonResponse
+    {
+        $developer = $this->developerService->getOne($id);
+
+        return self::createResponse(['data' => $developer], Response::HTTP_OK);
     }
 }
---
+++ src/Developer/DeveloperServiceInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CoenMooij\DevpoolApi\Developer;
+
+use CoenMooij\DevpoolApi\Authentication\User;
+use Illuminate\Database\Eloquent\Collection;
+
+interface DeveloperServiceInterface
+{
+    /**
+     * @return Developer[]|Collection
+     */
+    public function getAll(): Collection;
+
+    public function getOne(int $id): Developer;
+
+    public function createDeveloperFromUser(User $user): Developer;
+}
