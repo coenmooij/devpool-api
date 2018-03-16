19fff173dac960e6906b422ab13eb1a15f2d077f
Coen Mooij <coenmooij@gmail.com>
2018-03-16 11:42:31 +0100

    extract form from developer

--- src/Form/Question.php
+++ src/Form/Question.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace CoenMooij\DevpoolApi\Developer\Form;
+namespace CoenMooij\DevpoolApi\Form;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
